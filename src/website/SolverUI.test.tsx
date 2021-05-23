032b9b8e87094ac3e9f5ce7f1951187ef5ed8953
Vova Ivanov <jetvova@gmail.com>
2021-05-22 22:39:46 -0700

    Simplified and increased test coverage

--- src/website/SolverUI.test.tsx
+++ src/website/SolverUI.test.tsx
@@ -1,67 +1,119 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SolverUI from './SolverUI';
 import { PythagoreanSolver } from '../engine/solvers/math/pythagoreanSolver';
 
-it("renders empty textboxes correctly", () => {
-    const { queryByTestId } = render(<SolverUI solver={new PythagoreanSolver} />);
-    
-    expect(queryByTestId("textbox_A")).not.toBeNull();
-    expect(queryByTestId("textbox_B")).not.toBeNull();
-    expect(queryByTestId("textbox_C")).not.toBeNull();
-})
-
-it("renders var value correctly", () => {
+test("Var value rendered correctly", () => {
     const solver = new PythagoreanSolver;
     solver.A.value = 42;
     solver.B.value = 123.45;
-    const { queryByTestId } = render(<SolverUI solver={solver} />);
+    const { A, B, C } = renderSolverUI(solver);
 
-    expect((queryByTestId("textbox_A") as HTMLInputElement).value).toEqual("42");
-    expect((queryByTestId("textbox_B") as HTMLInputElement).value).toEqual("123.45");
-    expect((queryByTestId("textbox_C") as HTMLInputElement).value).toEqual("");
+    expect(A.value).toEqual("42");
+    expect(B.value).toEqual("123.45");
+    expect(C.value).toEqual("");
 })
 
-it("renders var checkbox correctly", () => {
+test("Var checkbox rendered correctly", () => {
     const solver = new PythagoreanSolver;
     solver.A.given = true;
     solver.B.given = false;
-    const { queryByTestId } = render(<SolverUI solver={solver} />);
+    const { checkA, checkB, checkC } = renderSolverUI(solver);
 
-    expect((queryByTestId("checkbox_A") as HTMLInputElement).checked).toBeTruthy();
-    expect((queryByTestId("checkbox_B") as HTMLInputElement).checked).toBeFalsy();
-    expect((queryByTestId("checkbox_C") as HTMLInputElement).checked).toBeFalsy();
+    expect(checkA.checked).toBeTruthy();
+    expect(checkB.checked).toBeFalsy();
+    expect(checkC.checked).toBeFalsy();
 })
 
-it("renders var value & checkbox correctly", () => {
+test("Var value & checkbox rendered correctly", () => {
     const solver = new PythagoreanSolver;
     solver.A.given = true;
     solver.B.given = false;
     solver.A.value = 42;
     solver.B.value = 123.45;
-    const { queryByTestId } = render(<SolverUI solver={solver} />);
+    const { A, B, C, checkA, checkB, checkC } = renderSolverUI(solver);
 
-    expect((queryByTestId("checkbox_A") as HTMLInputElement).checked).toBeTruthy();
-    expect((queryByTestId("checkbox_B") as HTMLInputElement).checked).toBeFalsy();
-    expect((queryByTestId("checkbox_C") as HTMLInputElement).checked).toBeFalsy();
+    expect(checkA.checked).toBeTruthy();
+    expect(checkB.checked).toBeFalsy();
+    expect(checkC.checked).toBeFalsy();
 
-    expect((queryByTestId("textbox_A") as HTMLInputElement).value).toEqual("42");
-    expect((queryByTestId("textbox_B") as HTMLInputElement).value).toEqual("123.45");
-    expect((queryByTestId("textbox_C") as HTMLInputElement).value).toEqual("");
+    expect(A.value).toEqual("42");
+    expect(B.value).toEqual("123.45");
+    expect(C.value).toEqual("");
 })
 
-it("renders a simple evaluation call correctly", () => {
-    const solver = new PythagoreanSolver;
-    solver.A.given = true;
-    solver.B.given = true;
-    solver.A.value = 3;
-    solver.B.value = 4;
-    const { queryByTestId, queryByPlaceholderText } = render(<SolverUI solver={solver} />);
+test("Variable becomes given when written", () => {
+    const { A, checkA } = renderSolverUI(new PythagoreanSolver);
     
-    const button = queryByTestId("button_solve");
-    expect(button as HTMLInputElement).toBeTruthy();   
-    fireEvent.click(button!);
-    
-    expect((queryByTestId("textbox_C") as HTMLInputElement).value).toEqual("5");
-    expect((queryByTestId("checkbox_C") as HTMLInputElement).checked).toBeFalsy();
+    expect(checkA.checked).toBeFalsy();
+    writeToTextBox(A, "30");
+    expect(checkA.checked).toBeTruthy();
+
 })
+test("Simple evaluation call correctly when pressing button", () => {
+    const { A, B, C, solve } = renderSolverUI(new PythagoreanSolver);
+    writeToTextBox(A, "30");
+    writeToTextBox(B, "40");
+    solve.click();
+    expect(C.value).toEqual("50");
+})
+
+test("Automatic call of solve upon change of focus", async () => {
+    const { A, B, C } = renderSolverUI(new PythagoreanSolver);    
+    writeToTextBox(A, "30");
+    writeToTextBox(B, "40");
+    C.focus();
+    expect(C.value).toEqual("50");
+})
+
+test("Variables get cleared when button is pressed", async () => {
+    const { A, B, C, clear } = renderSolverUI(new PythagoreanSolver);    
+    writeToTextBox(A, "30");
+    writeToTextBox(B, "40");
+    writeToTextBox(C, "50");
+
+    clear.click();
+    expect(A.value).toEqual("");
+    expect(B.value).toEqual("");
+    expect(C.value).toEqual("");
+})
+
+function writeToTextBox(textbox: HTMLInputElement, value: string) {
+    textbox.focus();
+    var current = "";
+    for (let c of value) {
+        const keyEvent = {code: "Digit" + c, key: c, charCode: c.charCodeAt(0)};
+        fireEvent.keyDown(textbox, keyEvent);
+        fireEvent.keyPress(textbox, keyEvent);
+        fireEvent.keyUp(textbox, keyEvent);
+        current += c;
+        fireEvent.change(textbox, {target: {value: current}});        
+    }    
+}
+
+interface PythagoreanUIControls {
+    A: HTMLInputElement;
+    B: HTMLInputElement;
+    C: HTMLInputElement;
+    checkA: HTMLInputElement;
+    checkB: HTMLInputElement;
+    checkC: HTMLInputElement;
+    solve: HTMLInputElement;
+    clear: HTMLInputElement;
+}
+
+function renderSolverUI(solver: PythagoreanSolver): PythagoreanUIControls {
+        const { queryByTestId } = render(<SolverUI solver={solver} />);
+    return { 
+        A: (queryByTestId("textbox_A") as HTMLInputElement),
+        B: (queryByTestId("textbox_B") as HTMLInputElement),
+        C: (queryByTestId("textbox_C") as HTMLInputElement),
+        checkA: (queryByTestId("checkbox_A") as HTMLInputElement),
+        checkB: (queryByTestId("checkbox_B") as HTMLInputElement),
+        checkC: (queryByTestId("checkbox_C") as HTMLInputElement),        
+        solve: (queryByTestId("button_solve") as HTMLInputElement),
+        clear: (queryByTestId("button_clear") as HTMLInputElement),
+    }
+}
+
